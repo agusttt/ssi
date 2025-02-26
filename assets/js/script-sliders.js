08cb138824b1c64f847599f2234de071c99e0e73
agusttt <agussusilo0893@gmail.com>
2025-02-27 01:53:01 +0700

    music audio and logo animate request

--- assets/js/script-sliders.js
+++ assets/js/script-sliders.js
@@ -110,22 +110,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-    const slidesContainer = document.querySelector(".slidesv");
-    const slidesver = document.querySelectorAll(".slidevsatu");
-    const slideWidth = slidesver[0].offsetWidth; // Get the width of a slide
-    let currentSlidev = 0;
-    let isTransitioning = false; // Flag to prevent overlapping transitions
-
-    // Clone the first and last slides for infinite scrolling
-    const firstClone = slidesver[0].cloneNode(true);
-    const lastClone = slidesver[slidesver.length - 1].cloneNode(true);
-    slidesContainer.appendChild(firstClone);
-    slidesContainer.insertBefore(lastClone, slidesver[0]);
-
-    // Update the width of the container to accommodate the cloned slides
-    slidesContainer.style.width = `${(slidesver.length + 2) * 100}%`;
-
     // Function to move to the next slide
     function nextSlidev() {
         if (isTransitioning) return; // Prevent overlapping transitions
@@ -177,20 +161,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     // SLIDE BOTTOM TO UP START ONE
-    const slidesContainerTwo = document.querySelector(".slidesv-two");
-    const slidesverTwo = document.querySelectorAll(".slidevsatu-two");
-    const slideHeight = slidesverTwo[0].offsetHeight; // Get the height of a slide
-    let currentSlideTwo = 0;
-    let isTransitioningTwo = false; // Flag to prevent overlapping transitions
+    // const slidesContainerTwo = document.querySelector(".slidesv-two");
+    // const slidesverTwo = document.querySelectorAll(".slidevsatu-two");
+    // const slideHeight = slidesverTwo[0].offsetHeight; // Get the height of a slide
+    // let currentSlideTwo = 0;
+    // let isTransitioningTwo = false; // Flag to prevent overlapping transitions
 
     // Clone the first and last slides for infinite scrolling
-    const firstCloneTwo = slidesverTwo[0].cloneNode(true);
-    const lastCloneTwo = slidesverTwo[slidesverTwo.length - 1].cloneNode(true);
-    slidesContainerTwo.appendChild(firstCloneTwo);
-    slidesContainerTwo.insertBefore(lastCloneTwo, slidesverTwo[0]);
+    // const firstCloneTwo = slidesverTwo[0].cloneNode(true);
+    // const lastCloneTwo = slidesverTwo[slidesverTwo.length - 1].cloneNode(true);
+    // slidesContainerTwo.appendChild(firstCloneTwo);
+    // slidesContainerTwo.insertBefore(lastCloneTwo, slidesverTwo[0]);
 
     // Update the height of the container to accommodate the cloned slides
-    slidesContainerTwo.style.height = `${(slidesverTwo.length + 2) * 100}vh`;
+    // slidesContainerTwo.style.height = `${(slidesverTwo.length + 2) * 100}vh`;
 
     // Function to move to the next slide
     function nextSlideTwo() {
@@ -240,6 +224,224 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(nextSlideTwo, 4900);
 
     // SLIDE BOTTOM TO UP END ONE
+
+
+     // SLIDER JS SLIDE COL 5 ROW 1 END START    
+     const slidehomecvron = document.querySelectorAll(".slidehcvron");
+     let currentShcvron = 0;
+   
+     function showShcvron(index) {
+         slidehomecvron.forEach((slidehcvron, i) => {
+         slidehcvron.classList.remove("active");
+         if (i === index) {
+         slidehcvron.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShcvron() {
+       currentShcvron = (currentShcvron + 1) % slidehomecvron.length;
+       showShcvron(currentShcvron);
+     }
+   
+     // Initial slide
+     showShcvron(currentShcvron);
+   
+     // Auto-looping
+     setInterval(nextShcvron, 4000); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 5 ROW 1 END 
+
+
+     // SLIDER JS SLIDE COL 4 ROW 2 END START    
+     const slidehomecfrtw = document.querySelectorAll(".slidehcfrtw");
+     let currentShcfrtw = 0;
+   
+     function showShcfrtw(index) {
+         slidehomecfrtw.forEach((slidehcfrtw, i) => {
+         slidehcfrtw.classList.remove("active");
+         if (i === index) {
+         slidehcfrtw.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShcfrtw() {
+       currentShcfrtw = (currentShcfrtw + 1) % slidehomecfrtw.length;
+       showShcfrtw(currentShcfrtw);
+     }
+   
+     // Initial slide
+     showShcfrtw(currentShcfrtw);
+   
+     // Auto-looping
+     setInterval(nextShcfrtw, 5600); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 4 ROW 2 END 
+
+     // SLIDER JS SLIDE COL 4 ROW 3 END START    
+     const slidehomecfrth = document.querySelectorAll(".slidehcfrth");
+     let currentShcfrth = 0;
+   
+     function showShcfrth(index) {
+         slidehomecfrth.forEach((slidehcfrth, i) => {
+         slidehcfrth.classList.remove("active");
+         if (i === index) {
+         slidehcfrth.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShcfrth() {
+       currentShcfrth = (currentShcfrth + 1) % slidehomecfrth.length;
+       showShcfrth(currentShcfrth);
+     }
+   
+     // Initial slide
+     showShcfrth(currentShcfrth);
+   
+     // Auto-looping
+     setInterval(nextShcfrth, 7000); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 4 ROW 3 END 
+
+
+
+     // SLIDER JS SLIDE COL 3 ROW 1 END START    
+     const slidehomeciron = document.querySelectorAll(".slidehciron");
+     let currentShciron = 0;
+   
+     function showShciron(index) {
+         slidehomeciron.forEach((slidehciron, i) => {
+         slidehciron.classList.remove("active");
+         if (i === index) {
+         slidehciron.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShciron() {
+       currentShciron = (currentShciron + 1) % slidehomeciron.length;
+       showShciron(currentShciron);
+     }
+   
+     // Initial slide
+     showShciron(currentShciron);
+   
+     // Auto-looping
+     setInterval(nextShciron, 10000); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 3 ROW 1 END 
+
+     // SLIDER JS SLIDE COL 3 ROW 2 END START    
+     const slidehomecirtw = document.querySelectorAll(".slidehcirtw");
+     let currentShcirtw = 0;
+   
+     function showShcirtw(index) {
+         slidehomecirtw.forEach((slidehcirtw, i) => {
+         slidehcirtw.classList.remove("active");
+         if (i === index) {
+         slidehcirtw.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShcirtw() {
+       currentShcirtw = (currentShcirtw + 1) % slidehomecirtw.length;
+       showShcirtw(currentShcirtw);
+     }
+   
+     // Initial slide
+     showShcirtw(currentShcirtw);
+   
+     // Auto-looping
+     setInterval(nextShcirtw, 8500); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 3 ROW 2 END 
+
+
+
+     // SLIDER JS SLIDE COL 2 ROW 1 END START    
+     const slidehomectron = document.querySelectorAll(".slidehctron");
+     let currentShctron = 0;
+   
+     function showShctron(index) {
+         slidehomectron.forEach((slidehctron, i) => {
+         slidehctron.classList.remove("active");
+         if (i === index) {
+         slidehctron.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShctron() {
+       currentShctron = (currentShctron + 1) % slidehomectron.length;
+       showShctron(currentShctron);
+     }
+   
+     // Initial slide
+     showShctron(currentShctron);
+   
+     // Auto-looping
+     setInterval(nextShctron, 6000); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 2 ROW 1 END 
+
+
+     // SLIDER JS SLIDE COL 1 ROW 2 END START    
+     const slidehomecortw = document.querySelectorAll(".slidehcortw");
+     let currentShcortw = 0;
+   
+     function showShcortw(index) {
+         slidehomecortw.forEach((slidehcortw, i) => {
+         slidehcortw.classList.remove("active");
+         if (i === index) {
+         slidehcortw.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShcortw() {
+       currentShcortw = (currentShcortw + 1) % slidehomecortw.length;
+       showShcortw(currentShcortw);
+     }
+   
+     // Initial slide
+     showShcortw(currentShcortw);
+   
+     // Auto-looping
+     setInterval(nextShcortw, 4500); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 1 ROW 2 END 
+
+
+
+     // SLIDER JS SLIDE COL 1 ROW 3 END START    
+     const slidehomecorth = document.querySelectorAll(".slidehcorth");
+     let currentShcorth = 0;
+   
+     function showShcorth(index) {
+         slidehomecorth.forEach((slidehcorth, i) => {
+         slidehcorth.classList.remove("active");
+         if (i === index) {
+         slidehcorth.classList.add("active");
+         }
+       });
+     }
+   
+     function nextShcorth() {
+       currentShcorth = (currentShcorth + 1) % slidehomecorth.length;
+       showShcorth(currentShcorth);
+     }
+   
+     // Initial slide
+     showShcorth(currentShcorth);
+   
+     // Auto-looping
+     setInterval(nextShcorth, 8000); // Change slide every 5 seconds
+ 
+     // SLIDER JS SLIDE COL 1 ROW 3 END END  
+
 
 
   });
